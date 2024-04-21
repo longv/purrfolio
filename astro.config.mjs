8bb55edf9b90c2525dcfdf7ebeefb4c25f07bd12
Long Vu <15876242+longv@users.noreply.github.com>
2024-04-22 00:26:41 +0300

    Make expertise summary

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-import icon from "astro-icon";
-
 import react from "@astrojs/react";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({

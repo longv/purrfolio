6709f1c8f053c7633280cb8f57aa29dd892329c0
Long Vu <15876242+longv@users.noreply.github.com>
2024-04-23 00:53:06 +0300

    Add certificate

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 import react from "@astrojs/react";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [mdx(), icon(), tailwind({
+  integrations: [mdx(), icon({
+    include: {
+      icons: ["coursera", "udacity-icon"],
+    }
+  }), tailwind({
     applyBaseStyles: false
   }), compress(), react()]
 });

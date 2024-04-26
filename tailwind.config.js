70ee7cf7a10780ada8bc0e5dc0febc8129e64aea
Long Vu <15876242+longv@users.noreply.github.com>
2024-04-26 21:50:37 +0300

    Fix darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
   variants: {
-    imageRendering: ['responsive'],
+    imageRendering: ["responsive"],
   },
   plugins: [
-    require('tailwindcss-image-rendering')(),
+    require("tailwindcss-image-rendering")(),
   ],
-  darkMode: ['class', '.darkmode'],
+  darkMode: ["class"],
 }

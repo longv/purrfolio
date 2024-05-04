bd4e9d476fa14445d92404ad378958d303f578cd
Long Vu <15876242+longv@users.noreply.github.com>
2024-05-04 17:11:39 +0300

    Nav bar for small screen

--- src/components/navigation/MainNavBar.tsx
+++ src/components/navigation/MainNavBar.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
+import cn from "classnames"
 import { SegmentedControl, IconButton } from "@radix-ui/themes"
-import { DoubleArrowUpIcon, DoubleArrowDownIcon } from "@radix-ui/react-icons"
+import { DoubleArrowUpIcon, DoubleArrowDownIcon, PersonIcon, BackpackIcon, MixIcon, RocketIcon, TargetIcon } from "@radix-ui/react-icons"
 
 const MainNavBar = () => {
   const [activeSection, setActiveSection] = useState<string>()
@@ -40,8 +41,13 @@ const MainNavBar = () => {
   }, [])
 
   return (
-    <div>
-      <div className={`${activeSection == "welcome" ? "visible" : "hidden"} transition-all ease-in-out delay-150 duration-300`}>
+    <div className="relative flex flex-col items-center">
+      <div
+        className={cn(
+          `${activeSection == "welcome" ? "visible" : "hidden"}`,
+          "transition-all ease-in-out delay-150 duration-300"
+        )}
+      >
         <IconButton
           className="animate-bounce"
           ml="1"
@@ -52,7 +58,12 @@ const MainNavBar = () => {
           <DoubleArrowDownIcon width="18" height="18" />
         </IconButton>
       </div>
-      <div className={`${activeSection != "welcome" ? "visible" : "hidden"} transition-all ease-in-out delay-150 duration-300`} >
+      <div
+        className={cn(
+          `${activeSection != "welcome" ? "max-sm:hidden" : "hidden"}`,
+          "transition-all ease-in-out delay-150 duration-500"
+        )}
+      >
         <SegmentedControl.Root
           radius="full"
           value={activeSection}
@@ -64,6 +75,61 @@ const MainNavBar = () => {
           <SegmentedControl.Item value="certificate">Certificates</SegmentedControl.Item>
           <SegmentedControl.Item value="radar">Tech radar</SegmentedControl.Item>
         </SegmentedControl.Root>
+        <IconButton
+          ml="1"
+          radius="full"
+          variant="surface"
+          onClick={() => navigateTo("welcome")}
+        >
+          <DoubleArrowUpIcon width="18" height="18" />
+        </IconButton>
+      </div >
+      <div
+        className={cn(
+          `${activeSection != "welcome" ? "sm:hidden" : "hidden"} absolute bottom-0 flex flex-row`,
+          "transition-all ease-in-out delay-150 duration-500"
+        )}
+      >
+        <IconButton
+          ml="1"
+          radius="full"
+          variant="surface"
+          onClick={() => navigateTo("about")}
+        >
+          <PersonIcon width="18" height="18" />
+        </IconButton>
+        <IconButton
+          ml="1"
+          radius="full"
+          variant="surface"
+          onClick={() => navigateTo("expertise")}
+        >
+          <BackpackIcon width="18" height="18" />
+        </IconButton>
+        <IconButton
+          ml="1"
+          radius="full"
+          variant="surface"
+          onClick={() => navigateTo("project")}
+        >
+          <MixIcon width="18" height="18" />
+        </IconButton>
+        <IconButton
+          ml="1"
+          radius="full"
+          variant="surface"
+          onClick={() => navigateTo("certificate")}
+        >
+          <RocketIcon width="18" height="18" />
+        </IconButton>
+        <IconButton
+          ml="1"
+          radius="full"
+          variant="surface"
+          onClick={() => navigateTo("radar")}
+        >
+          <TargetIcon width="18" height="18" />
+        </IconButton>
         <IconButton
           ml="1"
           radius="full"

7b8c724e489b0e9ab2e2af8441a33150a21387f6
Long Vu <15876242+longv@users.noreply.github.com>
2024-05-04 17:43:25 +0300

    Add site for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://longv.github.io",
   compressHTML: true,
   integrations: [
     mdx({

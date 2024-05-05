3038cb4a16a54bd54beaa56b05795dabbff75896
Long Vu <15876242+longv@users.noreply.github.com>
2024-05-06 00:15:06 +0300

    Small fix

--- src/components/intro/Bosses.tsx
+++ src/components/intro/Bosses.tsx
@@ -105,7 +105,7 @@ const Bosses = () => {
       >
         <span className="w-full">Art by tonobuu</span>
         <svg className="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
         </svg>
       </a>
     </div>
